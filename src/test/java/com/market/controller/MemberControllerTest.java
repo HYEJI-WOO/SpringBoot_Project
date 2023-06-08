ab65914056898e8a054a1fe2b7d6ff120392e5f5
HYEJI-WOO <whj1939@naver.com>
2023-06-08 13:05:27 +0900

    add

---
+++ src/test/java/com/market/controller/MemberControllerTest.java
@@ -0,0 +1,76 @@
+package com.market.controller;
+
+import com.market.constant.Gender;
+import com.market.dto.MemberFormDto;
+import com.market.entity.Member;
+import com.market.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+class MemberControllerTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Member createMember(String email, String password) {
+        MemberFormDto memberFormDto = new MemberFormDto();
+        memberFormDto.setEmail(email);
+        memberFormDto.setName("홍길동");
+        memberFormDto.setAddress1("서울시");
+        memberFormDto.setAddress2("마포구");
+        memberFormDto.setPhone("01012345678");
+        memberFormDto.setBirthYear(1955);
+        memberFormDto.setBirthMonth(10);
+        memberFormDto.setBirthDay(10);
+        memberFormDto.setGender(Gender.MALE);
+        memberFormDto.setPassword(password);
+        Member member = Member.createMember(memberFormDto, passwordEncoder);
+        return memberService.saveMember(member);
+    }
+
+    @Test
+    @DisplayName("로그인 성공 테스트")
+    public void loginSuccessTest() throws Exception {
+        String email = "test@email.com";
+        String password = "1234";
+        this.createMember(email, password);
+        mockMvc.perform(formLogin().userParameter("email")
+                .loginProcessingUrl("/members/login")
+                .user(email).password(password))
+                .andExpect(SecurityMockMvcResultMatchers.authenticated());
+    }
+
+    @Test
+    @DisplayName("로그인 실패 테스트")
+    public void loginFailTest() throws Exception {
+        String email = "test@email.com";
+        String password = "1234";
+        this.createMember(email, password);
+        mockMvc.perform(formLogin().userParameter("email")
+                        .loginProcessingUrl("/members/login")
+                        .user(email).password("12345"))
+                .andExpect(SecurityMockMvcResultMatchers.unauthenticated());
+    }
+
+}
---
+++ src/test/java/com/market/service/MemberServiceTest.java
@@ -0,0 +1,69 @@
+package com.market.service;
+
+import com.market.constant.Gender;
+import com.market.dto.MemberFormDto;
+import com.market.entity.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Member createMember() {
+        MemberFormDto memberFormDto = new MemberFormDto();
+        memberFormDto.setEmail("test@email.com");
+        memberFormDto.setName("홍길동");
+        memberFormDto.setAddress1("서울시 마포구 합정동");
+        memberFormDto.setAddress2("서울아파트 101동 512호");
+        memberFormDto.setPhone("01012345678");
+        memberFormDto.setBirthYear(1999);
+        memberFormDto.setBirthMonth(10);
+        memberFormDto.setBirthDay(30);
+        memberFormDto.setGender(Gender.FEMALE);
+        memberFormDto.setPassword("1234");
+        return Member.createMember(memberFormDto, passwordEncoder);
+    }
+
+    @Test
+    @DisplayName("회원가입 테스트")
+    public void saveMemberTest() {
+        Member member = createMember();
+        Member savedMember = memberService.saveMember(member);
+
+        assertEquals(member.getEmail(), savedMember.getEmail());
+        assertEquals(member.getPassword(), savedMember.getPassword());
+        assertEquals(member.getAddress1(), savedMember.getAddress1());
+        assertEquals(member.getRole(), savedMember.getRole());
+        assertEquals(member.getBirthDay(), savedMember.getBirthDay());
+    }
+
+    @Test
+    @DisplayName("중복 회원 가입 테스트")
+    public void saveDuplicateMemberTest() {
+        Member member1 = createMember();
+        Member member2 = createMember();
+        memberService.saveMember(member1);
+
+        Throwable e = assertThrows(IllegalStateException.class, () -> {
+            memberService.saveMember(member2);
+        });
+
+        assertEquals("이미 가입된 회원입니다.", e.getMessage());
+    }
+
+}
